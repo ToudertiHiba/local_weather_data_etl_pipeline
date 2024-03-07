030ab1d7a159aeab08530c3bcc77234ef00525be
Hiba TOUDERTI <hibatouderti@gmail.com>
2024-03-07 20:44:49 +0100

    Removing api data and updating kafka producer

--- kafka_producer.py
+++ kafka_producer.py
@@ -1,24 +1,32 @@
 
 from kafka import KafkaProducer
 import json
+import requests
 
 KAFKA_TOPIC = "weather"
 KAFKA_BROKER = "localhost:9092"
-API_KEY = "20229b2b71msh24e52dfd4f1c5f7p13a78cjsnad34c56afa3f"
-# Replace with the weather API URL
-API_URL = "https://weatherapi-com.p.rapidapi.com/current.json"
 
+class WeatherDataFetcher:
+    def __init__(self, api_key: str, api_url: str):
+        self.api_key = api_key
+        self.api_url = api_url
 
-def fetch_weather_data(city_name):
-    import requests
-    response = requests.get(API_URL, params={"q": city_name}, headers={
-                            "X-RapidAPI-Key": API_KEY})
-    return response.json()
-
+    def fetch_weather_data(self, city_name: str) -> dict:
+        response = requests.get(self.api_url, params={"q": city_name}, headers={
+                                "X-RapidAPI-Key": self.api_key})
+        response.raise_for_status()  # Raise an exception if the request fails
+        return response.json()
 
 def main():
     # List of cities for which you want weather data
     cities = ["Paris", "London", "New York", "Tokyo", "Sydney"]
+    with open('api_config.json') as f:
+        config = json.load(f)
+        weather_data_fetcher = WeatherDataFetcher(
+            api_key=config['API_KEY'],
+            api_url=config['API_URL']
+        )
+        
     producer = KafkaProducer(
         bootstrap_servers=KAFKA_BROKER,
         value_serializer=lambda v: json.dumps(v).encode("utf-8")
@@ -26,7 +34,7 @@ def main():
 
     while True:
         for city in cities:
-            weather_data = fetch_weather_data(city)
+            weather_data = weather_data_fetcher.fetch_weather_data(city)
             producer.send(KAFKA_TOPIC, value=weather_data)
             producer.flush()
 
